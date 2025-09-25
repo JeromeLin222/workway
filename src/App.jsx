ef2fd29e26620c45deb6029f98e718758ef10743
Jerome Lin <qsc002163@gmail.com>
2025-09-25 17:59:09 +0800

    feat: setup custom Tailwind theme base on design guidelines
    - Implement typography scale
    - Configure brand colors

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ function App() {
 
   return (
     <>
-      <h1 className='text-6xl text-red-900'>Hello Tailwind + React</h1>
+      <h1 className='text-6xl text-primary-900'>Hello Tailwind + React</h1>
+      <p className='text-title-3xl md:text-display text-secondary'>test Tailwind theme</p>
     </>
   )
 }
