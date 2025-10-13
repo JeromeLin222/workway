418731401ab513d1ba130acbdd152f66a4b930b4
Jerome Lin <qsc002163@gmail.com>
2025-10-13 14:09:47 +0800

    feat: add new images and SVGs for various components
    - Introduced multiple PNG and SVG images for the About, Index, and Service sections, enhancing visual content.
    - Added new icons and illustrations to improve user interface and experience across the application.
    - Updated HomePage component to include a new ButtonComponent for better navigation.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,34 @@
+import { ButtonComponent } from "../components/ui/Button";
+
 function HomePage() {
   return (
-    <div>
-      <h1 className="text-display">Home Page</h1>
-      <p className="text-xl">Home page under development. Stay tuned!</p>
-    </div>
-  );
+    <main>
+      <div>
+        <div className="max-w-7xl px-[clamp(1rem,4vw,4.5rem)] mx-auto flex justify-center items-center flex-col">
+          <div className="bg-[url(/images/index/gradient-ball.png)] bg-cover bg-top bg-no-repeat">
+            <h1 className="text-display text-center pt-20 mb-6">
+              一起陪伴、探索<br />
+              還有你那些未完成的夢
+            </h1>
+            <div className="flex justify-between mb-10">
+              <p className="text-natural-50">
+                自由工作很美，也很難。<br />
+                我們用 1 對 1 陪伴，陪你從第一步開始，直到夢想變成日常
+              </p>
+              <ButtonComponent
+                color="secondary"
+              >
+                <div className="flex">
+                  <p className="text-2xl font-bold mr-1">開始你的職旅</p> 
+                  <img src="/images/icon/chevron.svg"/>
+                </div>
+              </ButtonComponent>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  ); 
 }
 
 export default HomePage;
