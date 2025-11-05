642875632449d5a14aebfda6250266603aa87229
Jerome Lin <qsc002163@gmail.com>
2025-11-05 18:31:54 +0800

    feat: add CTA section to HomePage with contact information and new SVG icon
    - Introduced a new Call-to-Action (CTA) section on the HomePage to encourage user engagement.
    - Added contact details including phone and email, enhancing accessibility for users.
    - Included a new chevron right SVG icon for improved visual navigation within the CTA button.
    - Updated global styles for material icons to ensure consistent font sizing.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -198,6 +198,39 @@ function HomePage() {
             </div>
         </div>
       </div>
+      {/* CTA section */}
+      <div className="py-10">
+        <div className="container rounded-2xl bg-primary py-6 px-12">
+          <img className="mb-6" src="/images/logo-icon-w.svg"/>
+          <div className="border-b border-dashed border-white pb-6 mb-6">
+            <h2 className="text-white text-title-3xl md:text-title-4xl mb-3">邁向你真正想過的生活，從這裡開始</h2>
+            <p className="text-white text-body-md md:text-body-lg">第一次諮詢只要 30 分鐘，可能改變你未來 30 年的生活方式</p>
+          </div>
+          <div className="flex flex-col md:flex-row md:justify-between md:items-center">
+            <div className="text-white flex flex-col md:flex-row mb-6 md:mb-0">
+              <p className="mb-4 md:mb-0 md:mr-6 flex items-end">
+                <span class="material-symbols-outlined mr-1">call</span>
+                <p className="text-body-lg md:text-xl">0800-123-123</p>
+              </p>
+              <p className="flex items-end">
+                <span class="material-symbols-outlined mr-1">mail</span>
+                <p className="text-body-lg md:text-xl">Sample@mail.com</p>
+              </p>
+            </div>
+            <div className="self-start">
+              <ButtonComponent>
+                <div className="flex">
+                  <p>
+                    預約免費職涯探索
+                  </p>
+                  <span className="material-symbols-outlined">chevron_right</span>
+                </div>
+               
+              </ButtonComponent>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   ); 
 }
