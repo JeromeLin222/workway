a5d89cb1ae5f301eb1a5595c7d39aa1d4b71a7be
Jerome Lin <qsc002163@gmail.com>
2025-11-06 11:51:57 +0800

    feat: redesign ServicePage layout with new banner section and content
    - Replaced the existing layout with a new main structure featuring a banner section.
    - Added a title and descriptive text in Chinese to highlight service offerings.
    - Included an SVG image for enhanced visual appeal in the banner area.

--- src/pages/ServicePage.jsx
+++ src/pages/ServicePage.jsx
@@ -1,9 +1,15 @@
 function ServicePage() {
     return (
-      <div>
-        <h1 className="text-display text-primary">Service Page</h1>
-        <p className="text-xl">Home page under development. Stay tuned!</p>
-      </div>
+      <main>
+        {/* banner section */}
+        <div className="py-12 md:py-20 bg-[url(/images/service/banner-bg-pc.png)] bg-cover bg-center">
+          <div className="container text-center relative">
+            <h1 className="text-title-4xl md:text-title-5xl mb-6 md:mb-12">服務方案</h1>
+            <p className="text-body-md md:text-body-lg text-natural-50">我們最全面的服務方案，為您提供從評估、規劃到實踐的完整支持。不再擔心轉型過程中的各種挑戰，<br className="hidden md:block"/>我們的專家團隊將全程陪伴，確保您順利實現職業自由。</p>
+            <img className="hidden lg:block absolute bottom-[-88px] right-[128px]" src="/images/service/banner-person.svg" alt="banner person" />
+          </div>
+        </div>
+      </main>
     );
   }
   
