33de1fe784fdd9cc6cc7866e090be75b53a59a35
Jerome Lin <qsc002163@gmail.com>
2025-11-11 17:38:41 +0800

    feat: enhance ServicePage with new support plan section and layout improvements
    - Added a new section for "持續支持方案" with detailed information and a call-to-action button.
    - Improved layout structure for better responsiveness and visual appeal.
    - Updated background and image elements for enhanced user engagement.

--- src/pages/ServicePage.jsx
+++ src/pages/ServicePage.jsx
@@ -38,20 +38,49 @@ function ServicePage() {
         <div className="bg-natural-95 py-12 md:py-20">
           <div className="container">
             <div className="mb-6 md:md-12 flex flex-col items-center">
-                <img className="mb-1 w-[56px] h-[56px]" src="/images/service/conversation.svg" />
-                <h2 className="text-title-3xl md:text-title-4xl mb-4 md:mb-6">基礎諮詢服務</h2>
-                <p className="text-body-md md:text-body-lg text-natural-50">我們提供第一次免費諮詢，一起建立信任的橋樑</p>
-              </div>
-              <div className="flex flex-col gap-4 md:gap-6 md:grid grid-cols-3 ">
-                {consultPlanData.map((consultPlan) => (
-                  <ConsultPlanCard 
-                    key={consultPlan.id}
-                    {...consultPlan}
-                  />
-                ))}
-              </div>
+              <img className="mb-1 w-[56px] h-[56px]" src="/images/service/conversation.svg" />
+              <h2 className="text-title-3xl md:text-title-4xl mb-4 md:mb-6">基礎諮詢服務</h2>
+              <p className="text-body-md md:text-body-lg text-natural-50">我們提供第一次免費諮詢，一起建立信任的橋樑</p>
             </div>
-        </div> 
+            <div className="flex flex-col gap-4 md:gap-6 md:grid grid-cols-3 ">
+              {consultPlanData.map((consultPlan) => (
+                <ConsultPlanCard 
+                  key={consultPlan.id}
+                  {...consultPlan}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="py-12 md:py-20 relative">
+          <div className="container z-1 grid grid-cols-6">
+            <div className="mb-6 md:md-12 flex flex-col items-center col-span-4 col-start-2">
+              <img className="mb-1 w-[56px] h-[56px]" src="/images/service/love.svg" />
+              <h2 className="text-title-3xl md:text-title-4xl mb-4 md:mb-6">持續支持方案</h2>
+            </div>
+            <div className="flex flex-col col-span-6 md:col-span-4 md:col-start-2 relative items-center bg-secondary-90 rounded-2xl border-2 border-secondary py-6 md:py-12 overflow-hidden">
+              <div className="text-center z-2">
+                <h3 className="text-title-xl md:text-title-2xl font-medium mb-2">職旅會員計畫</h3>
+                <p className="text-body-sm md:text-body-md mb-12">享有長期社群支持與資源服務</p>
+                <p className="text-body-sm md:text-body-md mb-12">
+                  <span className="text-title-xl md:text-title-2xl text-primary mr-2">NT$ 9,800</span>
+                  /年
+                  <span className="ml-2">( NT$1,200 /月)</span>
+                </p>
+              </div>
+              <ButtonComponent>
+                <div className="flex">
+                  <p>我有興趣</p>
+                  <p className="material-symbols-outlined">chevron_right</p>
+                </div>
+              </ButtonComponent>
+              <img className="hidden lg:block absolute bottom-0 left-6" src="/images/service/subscribe-1.svg" alt="subscribe 1" />
+              <img className="hidden lg:block z-2 absolute bottom-0 right-6" src="/images/service/subscribe-2.svg" alt="subscribe 2" />
+              <div className="w-[600px] h-[160px] absolute top-0 right-0 rounded-tr-2xl bg-[url(/images/service/support-bg.png)] z-1"></div>
+            </div>
+          </div>
+          <div className="w-full bg-natural-95 h-[300px] absolute top-0 -z-10"></div>
+        </div>
       </main>
     );
   }
